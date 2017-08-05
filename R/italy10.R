f26b92a9b47abfcf9a360a1335eafd13bc9ed0d6
Rebecca Steorts <beka@stat.duke.edu>
2017-08-05 09:56:44 -0400

    Fixed R file documentation based on the babynames example to remove the warnings given.

--- R/italy10.R
+++ R/italy10.R
@@ -1,21 +1,22 @@
-#' Italian Survey on Household and Wealth, 2008
+#' Italian Survey on Household and Wealth, 2010
 #'
 #' The 2008 survey on covered 13,702 Italian Survey on Household and Wealth
 #' covering individuals. 
 #'
-#' @docType data
-#'
-#' @usage data(grav)
-#'
-#' @format A data frame with 11 variables: \code{parent}, \code{sex}, \code{anasc}, \code{nascreg}, 
-#' \code{cit}, \code{acom4c}, \code{studio}, code{q}, code{qual}, code{sett}, code{ireg}. There 
-#' is an additional variable \code{id} that is the unique identifier for the data set.
+#' @format A data frame with 11 variables: \code{parent}, \code{sex}, \code{anasc}, \code{nascreg}, ' \code{cit}, \code{acom4c}, \code{studio}, code{q}, code{qual}, code{sett}, code{ireg}. There is an additional variable \code{id} that is the unique identifier for the data set.
+
+#' Common models
+#' 
+#' This data set has one year's worth of data.
+#' This data set joined with another year's worth of 
+#' is appropriate for performing various types of record linkage
+#' and can be assessed by standard record linkage methods. 
 #'
 #' @keywords datasets, record linkage, entity resolution
 #'
 #' @references Steorts (2015), Bayesian Analysis
 #'
-#'
-#'
 #' @examples
-#' ATTN: Add in an example.
+#' head(italy10)
+#' dim(italy10)
+"italy10"
